01b2bf7c2ac57e4f2705bdfacbba72301711270d
Timothée Peignier <timothee.peignier@tryphon.org>
2012-03-16 17:55:11 +0100

    add base files

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='metrology',
+    version='0.1',
+    description='A library to easily measure what\'s going on in your python.',
+    author='Timothée Peignier',
+    author_email='timothee.peignier@tryphon.org',
+    url='https://github.com/cyberdelia/metrology',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'atomic',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
