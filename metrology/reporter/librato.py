5021a8c5db8b4be5741f170ecc72a26ad49bc75e
Timothée Peignier <timothee.peignier@tryphon.org>
2012-03-21 11:38:38 +0100

    small steps towards more reporter

--- metrology/reporter/librato.py
+++ metrology/reporter/librato.py
@@ -19,4 +19,53 @@ class LibratoReporter(Reporter):
 
     def write(self):
         for name, metric in self.registry:
+            if isinstance(metric, Meter):
+                self.send_metric(name, 'meter', metric, [
+                    'count', 'one_minute_rate', 'five_minute_rate',
+                    'fifteen_minute_rate', 'mean_rate'
+                ])
+            if isinstance(metric, Gauge):
+                self.send_metric(name, 'gauge', metric, [
+                    'value'
+                ])
+            if isinstance(metric, UtilizationTimer):
+                self.send_metric(name, 'timer', metric, [
+                    'count', 'one_minute_rate', 'five_minute_rate',
+                    'fifteen_minute_rate', 'mean_rate',
+                    'min', 'max', 'mean', 'stddev',
+                    'one_minute_utilization', 'five_minute_utilization',
+                    'fifteen_minute_utilization', 'mean_utilization'
+                ], [
+                    'median', 'percentile_95th'
+                ])
+            if isinstance(metric, Timer):
+                self.send_metric(name, 'timer', metric, [
+                    'count', 'one_minute_rate', 'five_minute_rate',
+                    'fifteen_minute_rate', 'mean_rate',
+                    'min', 'max', 'mean', 'stddev'
+                ], [
+                    'median', 'percentile_95th'
+                ])
+            if isinstance(metric, Counter):
+                self.send_metric(name, 'counter', metric, [
+                    'count'
+                ])
+            if isinstance(metric, Histogram):
+                sef.send_metric(name, 'histogram', metric, [
+                    'count', 'min', 'max', 'mean', 'stddev',
+                ], [
+                    'median', 'percentile_95th'
+                ])
+
+    def send_write(self, name, type, metric, keys, snapshot_keys=[]):
+        base_name = re.sub(r"\s+", "_", name)
+        if self.prefix:
+            base_name = "{0}.{1}".format(self.prefix, base_name)
+
+        for name in keys:
             pass
+
+        if hasattr(metric, 'snapshot'):
+            snapshot = metric.snapshot
+            for name in snapshot_keys:
+                pass
--- metrology/reporter/statsd.py
+++ metrology/reporter/statsd.py
@@ -1,16 +1,23 @@
-import logging
+import socket
 
 from metrology.instruments import *  # noqa
 from metrology.reporter.base import Reporter
 
 
 class StatsdReporter(Reporter):
-    def __init__(self, logger=None, level=logging.INFO, **options):
-        self.logger = logger
-        self.level = level
+    def __init__(self, host, port, **options):
+        self.host = host
+        self.port = port
+        self.addr = (host, port)
 
         self.prefix = options.get('prefix')
         super(StatsdReporter, self).__init__(**options)
+
+    @property
+    def socket(self):
+        if not self._socket:
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return self._socket
 
     def write(self):
         for name, metric in self.registry:
