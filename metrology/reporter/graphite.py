3d1f6df958be74d2e8dcfbbc5c74a83191569783
Damien Dubé <damien.dube@gmail.com>
2013-04-19 13:21:59 -0400

    Added graphite pickle protocol as well as buffering

--- metrology/reporter/graphite.py
+++ metrology/reporter/graphite.py
@@ -1,5 +1,7 @@
 import re
 import socket
+import pickle
+import struct
 from time import time
 
 from metrology.instruments import *  # noqa
@@ -23,7 +25,20 @@ class GraphiteReporter(Reporter):
         self.port = port
 
         self.prefix = options.get('prefix')
+        self.pickle = options.get('pickle', False)
+        self.batch_size = options.get('batch_size', 100)
+        if self.batch_size <= 0:
+            self.batch_size = 1
         super(GraphiteReporter, self).__init__(**options)
+        self.batch_count = 0
+        if self.pickle:
+            self._buffered_send_metric = self._buffered_pickle_send_metric
+            self._send = self._send_pickle
+            self.batch_buffer = []
+        else:
+            self._buffered_send_metric = self._buffered_plaintext_send_metric
+            self._send = self._send_plaintext
+            self.batch_buffer = ""
 
     @property
     def socket(self):
@@ -80,6 +95,9 @@ class GraphiteReporter(Reporter):
                         'median', 'percentile_95th'
                     ])
 
+        # Send metrics that might be in buffers
+        self._send()
+
     def send_metric(self, name, type, metric, keys, snapshot_keys=None):
         if snapshot_keys is None:
             snapshot_keys = []
@@ -88,15 +106,45 @@ class GraphiteReporter(Reporter):
             base_name = "{0}.{1}".format(self.prefix, base_name)
 
         for name in keys:
+            value = True
             value = getattr(metric, name)
-            self.socket.send("{0}.{1} {2} {3}\n\n".format(
-                base_name, name, value, int(time())
-            ))
+            self._buffered_send_metric(base_name, name, value, int(time()))
 
         if hasattr(metric, 'snapshot'):
             snapshot = metric.snapshot
             for name in snapshot_keys:
+                value = True
                 value = getattr(snapshot, name)
-                self.socket.send("{0}.{1} {2} {3}\n\n".format(
-                    base_name, name, value, int(time())
-                ))
+                self._buffered_send_metric(base_name, name, value, int(time()))
+
+    def _buffered_plaintext_send_metric(self, base_name, name, value, t, force=False):
+        self.batch_count += 1
+        self.batch_buffer += "{0}.{1} {2} {3}\n".format(
+            base_name, name, value, int(time()))
+        # Check if we reach batch size and send
+        if self.batch_count >= self.batch_size:
+            self._send_plaintext()
+
+    def _buffered_pickle_send_metric(self, base_name, name, value, t):
+        self.batch_count += 1
+        self.batch_buffer.append(("{0}.{1}".format(base_name, name), (t, value)))
+        # Check if we reach batch size and send
+        if self.batch_count >= self.batch_size:
+            self._send_pickle()
+
+    def _send_plaintext(self):
+        if len(self.batch_buffer):
+            self.socket.sendAll(self.batch_buffer + "\n")
+            # Reinitialze buffer and counter
+            self.batch_count = 0
+            self.batch_buffer = ""
+
+    def _send_pickle(self):
+        if len(self.batch_buffer):
+            payload = pickle.dumps(self.batch_buffer)
+            header = struct.pack("!L", len(payload))
+            message = header + payload
+            self.socket.sendAll(message)
+            # Reinitialze buffer and counter
+            self.batch_count = 0
+            self.batch_buffer = []
--- tests/reporter/test_graphite_pickle.py
+++ tests/reporter/test_graphite_pickle.py
@@ -20,7 +20,7 @@ class GraphiteReporterTest(TestCase):
 
     @patch.object(GraphiteReporter, 'socket')
     def test_send_nobatch(self, mock):
-        self.reporter = GraphiteReporter('localhost', 3333, batch_size=1)
+        self.reporter = GraphiteReporter('localhost', 3334, pickle=True, batch_size=1)
 
         Metrology.meter('meter').mark()
         Metrology.counter('counter').increment()
@@ -28,13 +28,13 @@ class GraphiteReporterTest(TestCase):
         Metrology.utilization_timer('utimer').update(1.5)
         Metrology.histogram('histogram').update(1.5)
         self.reporter.write()
-        self.assertTrue(mock.send.assert_called())
+        self.assertTrue(mock.sendAll.assert_called())
         self.assertEqual(50, len(mock.sendAll.call_args_list))
         self.reporter.stop()
 
     @patch.object(GraphiteReporter, 'socket')
     def test_send_batch(self, mock):
-        self.reporter = GraphiteReporter('localhost', 3333, batch_size=2)
+        self.reporter = GraphiteReporter('localhost', 3334, pickle=True, batch_size=2)
 
         Metrology.meter('meter').mark()
         Metrology.counter('counter').increment()
@@ -42,6 +42,6 @@ class GraphiteReporterTest(TestCase):
         Metrology.utilization_timer('utimer').update(1.5)
         Metrology.histogram('histogram').update(1.5)
         self.reporter.write()
-        self.assertTrue(mock.send.assert_called())
+        self.assertTrue(mock.sendAll.assert_called())
         self.assertEqual(25, len(mock.sendAll.call_args_list))
         self.reporter.stop()
