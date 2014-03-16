7a7c6af101fc6ea1dfeb892161cfa0213078cc34
Timothée Peignier <timothee.peignier@tryphon.org>
2014-03-15 21:45:50 -0700

    bump version

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Utilities',
     ]
 )
