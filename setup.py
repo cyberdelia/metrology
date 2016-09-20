b273fe76058cf24fddc1f68b4a0ca25e7e12146f
Timothée Peignier <timothee.peignier@tryphon.org>
2016-09-19 17:04:53 -0700

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='metrology',
-    version='1.2.0',
+    version='1.2.1',
     description='A library to easily measure what\'s going on in your python.',
     long_description=readme,
     author='Timothée Peignier',
